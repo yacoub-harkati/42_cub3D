dd2ad895105345255e0d4c80e4b0a4992cc23641
pleiadeslol <roumaissaezarhoune@gmail.com>
2024-12-07 23:38:35 +0100

    checked identifier order

--- src/check_data.c
+++ src/check_data.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 01:32:28 by root              #+#    #+#             */
-/*   Updated: 2024/12/07 22:33:32 by root             ###   ########.fr       */
+/*   Updated: 2024/12/07 23:36:27 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,53 @@ int	check_identifier(char **file, char *str)
 		return (1);
 }
 
+int	check_order(char **file)
+{
+	int	no;
+	int	so;
+	int	we;
+	int	ea;
+	int	f;
+	int	c;
+	int	i;
+
+	no = 0;
+	so = 0;
+	we = 0;
+	ea = 0;
+	f = 0;
+	c = 0;
+	i = 0;
+	while (file[i])
+	{
+		if (ft_strnstr(file[i], "NO", ft_strlen(file[i])))
+			no = i;
+		else if (ft_strnstr(file[i], "SO", ft_strlen(file[i])))
+			so = i;
+		else if (ft_strnstr(file[i], "WE", ft_strlen(file[i])))
+			we = i;
+		else if (ft_strnstr(file[i], "EA", ft_strlen(file[i])))
+			ea = i;
+		else if (ft_strnstr(file[i], "F", ft_strlen(file[i])))
+			f = i;
+		else if (ft_strnstr(file[i], "C", ft_strlen(file[i])))
+			c = i;
+		i++;
+	}
+	if (no > so || no > we || no > ea || no > f || no > c ||
+		so > we || so > ea || so > f || so > c || we > ea ||
+		we > f || we > c || ea > f || ea > c || f > c)
+		return (err("Error\nIdentifiers arent in right order\n"), 1);
+	return (0);
+}
+
 int	check_file(char **file)
 {
 	if (!check_identifier(file, "NO") || !check_identifier(file, "SO") ||
 		!check_identifier(file, "WE") || !check_identifier(file, "EA") ||
 		!check_identifier(file, "F") || !check_identifier(file, "C"))
 		return (1);
-	
+	if (check_order(file))
+		return (1);
 	return (0);
 }
