b262979d941d4055352675199262a893df409ee9
root <root@WIN-T3IBHH7P32N.>
2024-12-07 22:29:44 +0100

    checked if the iden is set more than one time

--- src/check_data.c
+++ src/check_data.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 01:32:28 by root              #+#    #+#             */
-/*   Updated: 2024/12/07 21:42:32 by root             ###   ########.fr       */
+/*   Updated: 2024/12/07 22:22:55 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,29 @@
 int	check_identifier(char **file, char *str)
 {
 	int	i;
+	int	count;
 
 	i = 0;
+	count = 0;
 	while (file[i])
 	{
 		if (ft_strnstr(file[i], str, ft_strlen(file[i])))
-			return (1);
+			count++;
 		i++;
 	}
-	return (err("Error\n"), err(str), err(" identifier doesnt exist\n"), 0);
+	if (count == 0)
+		return (err("Error\n"), err(str), err(" identifier doesnt exist\n"), 0);
+	else if (count > 1)
+		return (err("Error\n"), err(str), err(" identifier is set more than one time\n"), 0);
+	else
+		return (1);
 }
 
 int	check_file(char **file)
 {
 	if (!check_identifier(file, "NO") || !check_identifier(file, "SO") ||
-		!check_identifier(file, "WE") || !check_identifier(file, "EA"))
+		!check_identifier(file, "WE") || !check_identifier(file, "EA") ||
+		!check_identifier(file, "F") || !check_identifier(file, "C"))
 		return (1);
 	return (0);
 }
